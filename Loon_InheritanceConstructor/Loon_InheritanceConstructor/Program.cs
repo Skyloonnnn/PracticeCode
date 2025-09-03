de0b7d8d17446ad4b369148c31a761fa089606fa
ACT-STUDENT <ACT-STUDENT@LAB3-PC27>
2025-09-03 14:49:40 +0800

    Practice

---
+++ Loon_InheritanceConstructor/Loon_InheritanceConstructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_InheritanceConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantian object for Regular Student class
+            RegularStudent regularStudent = new RegularStudent("Aian cuyos", "BSIT", "IT306");
+            //Call the method to display Regular student info
+            regularStudent.RegularStudentInformaton();
+
+            //Instantian object for Irreegular Student class
+            IrregularStudent irregularStudent = new IrregularStudent("Sky Loon", "BSIT", 18);
+            //Call the method to display Irregular Student info
+            irregularStudent.IrregularStudentInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Loon_InheritanceConstructor/Loon_InheritanceConstructor/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_InheritanceConstructor
+{
+    internal class Student
+    {
+        //Declare fields for student
+        public string name;
+        public string program;
+
+        //Parameterized Constructor
+        public Student(string studentName, string studentProgram)
+        {
+            name = studentName;
+            program = studentProgram;
+        }
+
+        //Method to display student info
+        public void StudentInformaton()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n\tStudent Information");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\nStudent name: {name} \nProgram: {program}");
+        }
+    }
+
+    class RegularStudent : Student // derived class
+    {
+        //Declare fields for regular student
+        public string section;
+
+        //Parameterized Constructor
+        public RegularStudent(string studentName, string studentProgram, string studentSection) : base(studentName, studentProgram)
+        {
+            section = studentSection;
+        }
+
+        //Method to display Regular Student info
+        public void RegularStudentInformaton()
+        {
+            StudentInformaton();
+            Console.WriteLine($"Section: {section}");
+        }
+    }
+
+    class IrregularStudent : Student //derived class
+    {
+        //Declare fields for Irregular Student 
+        public int units;
+
+        //Parameterized Constructor
+        public IrregularStudent(string studentName, string studentProgram, int unitsEnrolled) : base(studentName, studentProgram)
+        {
+            units = unitsEnrolled;
+        }
+
+        //Method to display Irregular Student info
+        public void IrregularStudentInfo()
+        {
+            StudentInformaton();
+            Console.WriteLine($"Enrolled Units: {units}");
+        }
+    }
+}
