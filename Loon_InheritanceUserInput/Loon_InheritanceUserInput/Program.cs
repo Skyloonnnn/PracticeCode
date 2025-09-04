284cb4af632101b3c8f14abdd93085655ec50eb9
ACT-STUDENT <ACT-STUDENT@LAB4-PC39>
2025-09-04 13:45:24 +0800

    UseInput

---
+++ Loon_InheritanceUserInput/Loon_InheritanceUserInput/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_InheritanceUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            Console.WriteLine("Select shoes that you want");
+            Console.WriteLine("");
+
+            string brandName = "";
+
+            Console.Write("\n1. Nike \n2. Adidas \n3. Puma \nPick a numer of brand you want: ");
+            string brandChoice = Console.ReadLine();
+
+            if (brandChoice == "1")
+            {
+                brandName = "Nike";
+            }
+            else if (brandChoice == "2")
+            {
+                brandName = "Adidas";
+            }
+            else if (brandChoice == "3")
+            {
+                brandName = "Puma";
+            }
+            else
+            {
+                Console.WriteLine("Please select brand!!");
+                goto Main;
+            }
+
+            Console.Write("What model of shoes you want: ");
+            string shoesModel = Console.ReadLine();
+
+            Shoes shoes = new Shoes(brandName, shoesModel);
+
+            
+
+
+
+
+            shoes.ShoeInfo();
+
+            Console.ReadKey();
+            
+
+            
+        }
+    }
+}
---
+++ Loon_InheritanceUserInput/Loon_InheritanceUserInput/Shoes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_InheritanceUserInput
+{
+    class Shoes
+    {
+        public string brand, model;
+
+        public Shoes(string brandName, string shoeModel)
+        {
+            brand = brandName;
+            model = shoeModel;
+        }
+
+        public void ShoeInfo()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("\tHere are the shoes you want");
+            Console.WriteLine($"Shoe Brand: {brand} \nShoe Model: {model}");
+        }
+
+    }
+    class ShoesSize : Shoes
+    {
+        public int size;
+
+        public ShoesSize(string brandName, string shoeModel, int shoeSize) : base(brandName, shoeModel)
+        {
+            ShoeInfo();
+            size = shoeSize;
+        }
+
+        public void SizeInfo()
+        {
+            Console.WriteLine($"Shoe size: {size}");
+        }
+    }
+}
