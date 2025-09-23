cee4a809ceaf7ce0b88ce36deaa4f03beb3989eb
My Laptop <My Laptop@DESKTOP-0U8QNKN>
2025-09-23 21:14:08 +0800

    Practice

---
+++ Polymorphism_Practice/Polymorphism_Practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shirt polo = new PoloShirt { Price = 450, Discount = 100 };
+            Shirt sando = new Sando { Price = 600, Discount = 150 };
+
+            Console.WriteLine($"Here's the final PoloShirt price: {polo.DiscontedPrice}");
+            Console.WriteLine($"\nHere's the final Sando price: {sando.DiscontedPrice}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Polymorphism_Practice/Polymorphism_Practice/Shirt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Practice
+{
+    class Shirt
+    {
+        public double Discount { get; set; }
+
+        public double Price { get; set; }
+
+        public virtual double DiscontedPrice
+        {
+            get { return Price - Discount; }
+        }
+    }
+
+    class PoloShirt : Shirt
+    {
+        public override double DiscontedPrice
+        {
+            get { return Price - Discount; }
+        }
+    }
+
+    class Sando : Shirt
+    {
+        public override double DiscontedPrice
+        {
+            get { return Price - Discount; }
+        }
+    }
+}
