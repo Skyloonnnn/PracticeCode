2257dcf03882b35c51ade61033c844fed7690425
My Laptop <My Laptop@DESKTOP-0U8QNKN>
2025-09-23 22:52:59 +0800

    Practice3

---
+++ PolymorphismPractice3/PolymorphismPractice3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here are the avaiable type of shirt: \n1.PoloShirt \n2.Sando");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Input only numbers!!");
+            Console.ResetColor();
+            Console.Write("Select what type of shirt you want: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (choice == 1)
+            {
+                Console.Write("\nEnter the brand of Poloshirt you want: ");
+                string brand = Console.ReadLine();
+
+                Console.Write("Enter the price of that PoloShirt: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+                
+                PoloShirt shirt = new PoloShirt();
+
+                shirt.Brand = brand;
+                shirt.Price = price;
+
+                shirt.DiscountedPriceDisplay();
+            }
+            else if (choice == 2)
+            {
+                Console.Write("\nEnter what type of Sando you want: ");
+                string type = Console.ReadLine();
+
+                Console.Write("Enter the price of that Sando: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                Sando sando = new Sando();
+
+                sando.Type = type;
+                sando.Price = price;
+
+                sando.DiscountedPriceDisplay();
+            }else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
---
+++ PolymorphismPractice3/PolymorphismPractice3/Shirt.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice3
+{
+    class Shirt
+    {
+        public double Price { get; set; }
+
+       
+        public virtual double CalculatePrice()
+        {
+            double discount = Price * 0.35;
+            return Price - discount;
+        }
+
+        public virtual void DiscountedPriceDisplay()
+        {
+            Console.WriteLine($"Final Price: {CalculatePrice():.00}");
+        }
+    }
+
+    class PoloShirt : Shirt
+    {
+        public double Discount { get; set; }
+
+        public string Brand { get; set; }
+        public override double CalculatePrice()
+        {
+            
+            Discount = Price * 0.45;
+            return Price - Discount;
+        }
+
+        public override void DiscountedPriceDisplay()
+        {
+            Console.WriteLine($"\nThe brand of the PoloShirt: {Brand}");
+            Console.WriteLine($"Here's the final PoloShirt price: {CalculatePrice():.00}");
+            Console.WriteLine($"\nCalculation of discount: \n{Price} - {Discount} = {CalculatePrice()}");
+        }
+    }
+
+    class Sando : Shirt
+    {
+        public double Discount { get; set ; }
+        public string Type { get; set; }
+
+        public override double CalculatePrice()
+        {
+            Discount = Price * 0.50;
+            return Price - Discount;
+
+        }
+
+        public override void DiscountedPriceDisplay()
+        {
+            Console.WriteLine($"\nThe type of Sando: {Type}");
+            Console.WriteLine($"Here's the final Sando price: {CalculatePrice():.00}");
+            Console.WriteLine($"\nCalculation of discount: \n{Price} - {Discount} = {CalculatePrice()}");
+        }
+    }
+}
