dea0a526a42efc2fca909eb8bd99040544a9b731
My Laptop <My Laptop@DESKTOP-0U8QNKN>
2025-09-23 22:11:24 +0800

    Practice2

---
+++ PolymorphismPractice2/PolymorphismPractice2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Here's the available shirt: \n1.Poloshirt \n2.Sando \nSelect type of shirt you want: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the price of shirt you want: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Shirt shirt = new PoloShirt();
+                shirt.Price = price;
+
+                shirt.DiscountedPriceDisplay();
+
+            }
+            else if (choice == 2)
+            {
+                Shirt shirt = new Sando();
+                shirt.Price= price;
+
+                shirt.DiscountedPriceDisplay();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+                Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ PolymorphismPractice2/PolymorphismPractice2/Shirt.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice2
+{
+    class Shirt
+    {
+        
+
+        public double Price { get; set; }
+
+        public virtual double CalculatePrice()
+        {
+            double discount = Price * 0.35;
+             return Price - discount;
+        }
+
+        public virtual void DiscountedPriceDisplay()
+        {
+            Console.WriteLine($"Final Price: {CalculatePrice():.00}");
+        }
+
+    }
+
+    class PoloShirt : Shirt
+    {
+        public double discount;
+        
+        public override double CalculatePrice()
+        {
+            
+            discount = Price * 0.45;
+            return Price - discount;
+        }
+
+        public override void DiscountedPriceDisplay()
+        {
+            Console.WriteLine($"\nHere's the final PoloShirt price: {CalculatePrice():.00}");
+            Console.WriteLine($"\nCalculation of discount: \n{Price} - {discount} = {CalculatePrice()}");
+        }
+    }
+
+    class Sando : Shirt
+    {
+        public double discount;
+        
+        public override double CalculatePrice()
+        { 
+            discount = Price * 0.50;
+            return Price - discount;
+           
+        }
+
+        public override void DiscountedPriceDisplay()
+        { 
+            Console.WriteLine($"\nHere's the final Sando price: {CalculatePrice():.00}");
+            Console.WriteLine($"\nCalculation of discount: \n{Price} - {discount} = {CalculatePrice()}");
+        }
+    }
+}
+
+
